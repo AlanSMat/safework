58f74d1da400e8026b0ce32fb24c21a11bdb9656
AlanSMat <steve@asmather.com>
2021-09-06 12:54:58 +1000

    minor change to page header

--- page.php
+++ page.php
@@ -6,20 +6,8 @@
  */
 
 get_header();
-
 ?>
-<style>
-        .x {
-            /* background-color:#fff; */
-            background-image: url('woman-hat.jpg');
-            height: 250px;
-        }
-</style>
-<div class="x">
-	xx
-</div>
 <?php
-
 get_template_part('template-parts/content');
 
 get_footer();
